6583aaae676bf460eafb666456010478ef94a791
Sav Younts <savyounts@gmail.com>
2018-08-04 23:33:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   key_for_min = nil
   default_value = nil
   name_hash.each do |key, value|
-    if value < default_value || default_value == nil
+    if default_value == nil || value < default_value 
+      default_value = value
       key_for_min = key 
     end
   end
